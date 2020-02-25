fa59cbb1f03d4d8175f8bf0bd4896e922e5bca18
Matt Lorber <matthew.lorber@outlook.com>
2020-02-25 15:06:43 -0600

    complete

--- index.js
+++ index.js
@@ -1,71 +1,112 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const cw = canvas.width;
-const w = cw / 20;
-const h = w;
+const img = document.getElementById("apple"); // apple image
+const canvas = document.getElementById("canvas"); // canvas (game board)
+const ctx = canvas.getContext("2d"); // using 2d canvas
+const cw = canvas.width; // defined in html (400 x 400)
+const w = cw / 20; // since the grid is square, it's dimensions are w x w
+const apple = {x:280,y:200}; // apple's initial position
+const snake = [[120,200],[100,200],[80,200],[60,200]]; // snake's initial position
+let dir = 39; // holds direction snake is heading as a keycode (e.g. 39 is right, corresponding to right arrow key)
+let mi = 250; // move interval (time in ms between snake moves)
+let ll = 4; // number of body segments for the snake
 
-function drawGrid() {
-    ctx.strokeStyle = "black";
-    for (var x=0; x<=cw; x+=w) {
-        ctx.moveTo(x,0);
-        ctx.lineTo(x,cw);
-    }
-    for (var x=0; x<=cw; x+=w) {
-        ctx.moveTo(0,x);
-        ctx.lineTo(cw,x);
-    }
-    ctx.stroke();
+// KEYBOARD LISTENER
+document.onkeydown = move; // listen for keyboard
+
+// RANDOM POSITION
+// gets a random xy position in grid to correspond to cell's top-left corner
+function rand(cb) {
+    var cb = {
+        x:(w*Math.floor(Math.random()*w)),
+        y:(w*(Math.floor(Math.random()*w)))
+    };
+    return cb;
 }
-drawGrid();
+
+// GAME SETUP
+function setup() {
+    drawApple();
+    // DRAW SNAKE
+    ctx.fillStyle = "green"; // snake color
+    for (let i=0; i<snake.length; i++) {
+        ctx.fillRect(snake[i][0],snake[i][1],w,w); // draw snake
+    }
+}
+setup();
+
+function drawApple() {
+    // DRAW APPLE
+    ctx.drawImage(img,apple.x,apple.y); // draw apple
+}
 
 
+// MOVEMENT
+let mm = setInterval(move,mi); // start the snake moving
 
-function makeSnake() {
+
+function move(e) {
+    if (e === undefined) {e = dir;} else {e = e.keyCode}
+    
+    var dx=0, dy=0;
+    switch (e) {
+        case 37: dx = -20; if (dir === 39) {return;} else {dir = 37}; break;
+        case 38: dy = -20; if (dir === 40) {return;} else {dir = 38}; break;
+        case 39: dx = 20; if (dir === 37) {return;} else {dir = 39}; break;
+        case 40: dy = 20; if (dir === 38) {return;} else {dir = 40}; break;
+    }
+
+    ctx.fillStyle = "#111";
+    ctx.fillRect(snake[snake.length-1][0],snake[snake.length-1][1],w,w); // erase last segment as snake moves on
+
+    snake.splice(0, 0, [snake[0][0] + dx, snake[0][1] + dy]); // add new head to 0 on snake array
+    snake.pop(); // remove snake last segment
+
     ctx.fillStyle = "green";
-    // random starting point for head and head position going forward is x,y
-    let x,y;
-    let xrand = x = w*(Math.floor(Math.random()*w));
-    let yrand = y = w*(Math.floor(Math.random()*w));
-    // head
-    fill(xrand,yrand);
-    document.onkeydown = moveHead;
+    ctx.fillRect(snake[0][0],snake[0][1],w,w); // draw new snake head
 
-    function moveHead(e) {
-        ctx.fillStyle = "green";
-        let dx=0;
-        let dy=0;
-        switch (e.keyCode) {
-            case 37: dx = -20; break;
-            case 38: dy = -20; break;
-            case 39: dx = 20; break;
-            case 40: dy = 20; break;
+    // CHECK FOR EATING AN APPLE (i.e. scoring)
+    if (snake[0][0] === apple.x && snake[0][1] === apple.y) { // if snake head coincides with an apple, score & lengthen
+        let score = Number(document.getElementById("apples").innerHTML); // score at top-left of gameboard
+        score += 1; // increment score
+        document.getElementById("apples").innerHTML = score; // write new score
+        newApple(); // place a new, random apple
+        snake.push([snake[snake.length-1][0],snake[snake.length-1][1]]); // make snake longer by doubling last element
+        mi -= 5; // decrement move interval counter (used later to speed up snake)
+        ll += 1; // increase snake length counter
+        clearInterval(mm);
+        mm = setInterval(move,mi);
+        document.getElementById("tt").innerHTML = mi;
+        document.getElementById("ll").innerHTML = ll;
+    }
+}
+
+// NEW APPLE
+function newApple() {
+    let newLoc = rand();
+    for (let i=0; i<ll; i++) {
+        if (snake[i][0] === newLoc.x && snake[i][1] === newLoc.y) {
+            newLoc = rand();
+            i = 0;
         }
-// console.log(dx,dy);
-        setInterval(fill(dx,dy),500);
-        
     }
+    apple.x = newLoc.x;
+    apple.y = newLoc.y;
+    drawApple();
+}
 
-    function fill(dx,dy) {
-            x+=dx;
-            y+=dy;
-            if (x >= 400) {x=0;}
-            if (y >= 400) {y=0;}
-            if (x <= -20) {x=380;}
-            if (y <= -20) {y=380;}
-            ctx.fillRect(x,y,w,h);
-        
-        // setInterval(snake,500);
+// CHECK FOR COLLISIONS (SELF-INTERSECT OR WALL)
+let int = setInterval(()=>{
+    for (let i=1; i<snake.length; i++) {
+        if (snake[0][0] === snake[i][0] && snake[0][1] === snake[i][1]) {endgame();return}
+        if (snake[0][0] <= -20 || snake[0][0] >= 400) {endgame();return}
+        if (snake[0][1] <= -20 || snake[0][1] >= 400) {endgame();return}
     }
-    
-    
-}
-makeSnake();
-//setInterval(placeApple,500);
+},100);
 
-
-// Random placement of the apple (w,h is size) within the grid (x,y,w,h)
-function placeApple() {
-    ctx.fillStyle = "#ff0000";
-    ctx.fillRect(w*(Math.floor(Math.random()*w)), w*(Math.floor(Math.random()*w)), w, h);
+// END GAME
+function endgame() {
+    moving = false;
+    document.onkeydown = null;
+    clearInterval(int);
+    clearInterval(mm);
 }
-placeApple();
+
